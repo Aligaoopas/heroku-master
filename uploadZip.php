b19929a999782e57e4bb8d63b62d87b3c10ac9c2
Aligaoopas <Aligaoopas@github.com>
2018-01-11 14:44:02 +0100

    re

--- uploadZip.php
+++ uploadZip.php
@@ -7,6 +7,46 @@
  * - check the upload max size limit
  */
 
+class UploadException extends Exception
+{
+    public function __construct($code) {
+        $message = $this->codeToMessage($code);
+        parent::__construct($message, $code);
+    }
+
+    private function codeToMessage($code)
+    {
+        switch ($code) {
+            case UPLOAD_ERR_INI_SIZE:
+                $message = "The uploaded file exceeds the upload_max_filesize directive in php.ini";
+                break;
+            case UPLOAD_ERR_FORM_SIZE:
+                $message = "The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form";
+                break;
+            case UPLOAD_ERR_PARTIAL:
+                $message = "The uploaded file was only partially uploaded";
+                break;
+            case UPLOAD_ERR_NO_FILE:
+                $message = "No file was uploaded";
+                break;
+            case UPLOAD_ERR_NO_TMP_DIR:
+                $message = "Missing a temporary folder";
+                break;
+            case UPLOAD_ERR_CANT_WRITE:
+                $message = "Failed to write file to disk";
+                break;
+            case UPLOAD_ERR_EXTENSION:
+                $message = "File upload stopped by extension";
+                break;
+
+            default:
+                $message = "Unknown upload error";
+                break;
+        }
+        return $message;
+    }
+}
+
 // if all went fine ===>
 
 // 1) create a NEW UNIQUE uploaded zip filename
@@ -73,4 +113,6 @@
 header('Content-Type: application/json');
 
 // 23) finally return error message or the array of images
+var_dump($_FILES);
 echo json_encode($_FILES);
+
